dd72f08f0559a553946d706477d01bee83585b57
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2020-02-07 21:17:28 -0300

    info circuital de artigas

--- DataBases/Elecciones2019/VotosPorCircuito.R
+++ DataBases/Elecciones2019/VotosPorCircuito.R
@@ -4,8 +4,8 @@
 
 ## Librerías usadas -----------
 
-library(tidyverse)
-library(magrittr)
+library(tidyverse, quietly = TRUE)
+library(magrittr, quietly = TRUE)
 
 ## Bases de datos de la Corte Electoral ---------------
 
@@ -84,7 +84,136 @@ escrutinio %<>%
 
 ## Información de los circuitos ----------------
 
+file_list <- str_subset(
+   string = list.files(
+      path = here::here("/DataBases/Elecciones2019")
+   ),
+   pattern = ".pdf$"
+)
 
+header <- "Circuito Serie Desde Hasta Habilitados Localidad Direccion Accesibilidad —_urb/rural"
+
+info_circuitos <- pdftools::pdf_ocr_text(
+   pdf = paste0(here::here("/DataBases/Elecciones2019"), "/", file_list[1])
+) %>%
+   str_split(
+      pattern = "\n"
+   ) %>%
+   unlist() %>%
+   tibble::enframe(
+      name = NULL
+   ) %>%
+   mutate(
+      value = str_trim(
+         string = value,
+         side = "both"
+      )
+   ) %>%
+   filter(
+      !str_detect(
+         string = value,
+         pattern = "ELECCIONES NACIONALES 2019"
+      ),
+      !str_detect(
+         string = value,
+         pattern = "ae CORTE ELECTORAL"
+      ),
+      !str_detect(
+         string = value,
+         pattern = "Republica Oriental del Uruguay"
+      ),
+      !str_detect(
+         string = value,
+         pattern = "ELECCIONES NACIONALES"
+      ),
+      !str_detect(
+         string = value,
+         pattern = header
+      )
+   ) %>%
+   mutate(
+      departamento = str_to_lower(str_remove(file_list[1], ".pdf")),
+      circuito = str_extract(
+         string = value,
+         pattern = "^([:digit:]+)\\b"
+      ),
+      value = str_remove(
+         string = value,
+         pattern = "^([:digit:]+)\\b\\s"
+      ),
+      serie = str_extract(
+         string = value,
+         pattern = "^([A-Z]{3})\\b"
+      ),
+      value = str_remove(
+         string = value,
+         pattern = "^([A-Z]{3})\\b\\s"
+      ),
+      desde = str_extract(
+         string = value,
+         pattern = "^([0-9]+)\\b"
+      ),
+      value = str_remove(
+         string = value,
+         pattern = "^([0-9]+)\\b\\s"
+      ),
+      hasta = str_extract(
+         string = value,
+         pattern = "^([0-9]+)\\b"
+      ),
+      value = str_remove(
+         string = value,
+         pattern = "^([0-9]+)\\b\\s"
+      ),
+      habilitados = str_extract(
+         string = value,
+         pattern = "^([0-9]+)\\b"
+      ),
+      value = str_remove(
+         string = value,
+         pattern = "^([0-9]+)\\b\\s"
+      ),
+      urbano = str_extract(
+         string = value,
+         pattern = "(rural|urb)$"
+      ),
+      value = str_remove(
+         string = value,
+         pattern = "\\s(rural|urb)$"
+      ),
+      accesibilidad = str_extract(
+         string = value,
+         pattern = "\\b(S|N)$"
+      ),
+      value = str_remove(
+         string = value,
+         pattern = "\\s(S|N)$"
+      ),
+      localidad = str_extract(
+         string = value,
+         pattern = "^([A-Za-z]+)\\b"
+      ),
+      value = str_remove(
+         string = value,
+         pattern = "^([A-Za-z]+)\\b\\s"
+      ),
+      value = str_trim(
+         string = value,
+         side = "both"
+      )
+   ) %>%
+   transmute(
+      departamento,
+      circuito = as.numeric(circuito),
+      serie,
+      desde = as.numeric(desde),
+      hasta = as.numeric(hasta),
+      habilitados = as.numeric(habilitados),
+      localidad,
+      direccion = value,
+      accesibilidad,
+      urbano
+   )
 
 #===============#
 #### THE END ####
