facdfb19cad965e31060a2d17df7d8e7b8b79663
Leandro Zipitria <leandro.zipitria@gmail.com>
2020-03-08 23:59:58 -0300

    nuevas bases y R files para crear mapa

---
+++ DataBases/Censo2011/Censo2011Personas-final.R
@@ -0,0 +1,39 @@
+library(rgdal)
+library(dplyr)
+library(leaflet)
+library(sf)
+
+
+setwd("c://Users/leandro/Dropbox/Docs/1imagen-data/ine/")
+setwd("~/Dropbox/Docs/1imagen-data/ine/")
+
+
+prueba <- read_sf("basefinal.shp")
+
+
+#### ----------------- Grafico -----------------
+
+cuts <- quantile(prueba$pob, probs = seq(0,1, by=0.2))
+pal <- colorBin("YlOrRd", domain = prueba$pob, bins = cuts)
+
+
+m <- leaflet(prueba) %>%
+   addTiles() %>%
+   setView(-56.1, -32, zoom = 6)  %>%
+   addPolygons(
+      fillColor = ~pal(pob),
+      weight = 2,
+      opacity = 1,
+      color = "white",
+      dashArray = "3",
+      fillOpacity = 0.7,
+      highlight = highlightOptions(
+         weight = 5,
+         color = "#666",
+         dashArray = "",
+         fillOpacity = 0.7,
+         bringToFront = TRUE)) %>%
+   addLegend(pal = pal, values = ~pob, opacity = 0.7, title = NULL,
+             position = "bottomright")
+m
+
