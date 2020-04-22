ce019ea816a59648199967a8b524dcd02c5c9560
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2020-04-21 23:12:55 -0300

    reads data set

---
+++ DataBases/EUTIC/eutic.R
@@ -0,0 +1,9 @@
+#=============#
+#### EUTIC ####
+#=============#
+
+haven::read_sav(file = "DataBases/EUTIC/BASE HOGARES Y PERSONAS EUTIC 2016.sav")
+
+#===============#
+#### THE END ####
+#===============#
