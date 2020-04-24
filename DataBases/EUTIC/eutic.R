06dd9c296f2324972b796347b295e7db14bc3e77
Daniel Czarnievicz <daniel.czarnievicz@gmail.com>
2020-04-23 22:17:24 -0300

    parsea variables: - nper - peso - sexo - edad - uso_celular - uso_smart_phone

--- DataBases/EUTIC/eutic.R
+++ DataBases/EUTIC/eutic.R
@@ -2,7 +2,53 @@
 #### EUTIC ####
 #=============#
 
-haven::read_sav(file = "DataBases/EUTIC/BASE HOGARES Y PERSONAS EUTIC 2016.sav")
+library(magrittr)
+library(tidyverse)
+
+# Carga datos -------------------------------------------------------------
+eutic <- haven::read_sav(
+   file = here::here("DataBases/EUTIC/BASE HOGARES Y PERSONAS EUTIC 2016.sav")
+) %>%
+   dplyr::rename_all(
+      .funs = tolower
+   )
+
+
+# Construye objeto para el App --------------------------------------------
+eutic %>%
+   # filter(
+   #    p23_1 == 0
+   # ) %>%
+   dplyr::transmute(
+      nper = dplyr::row_number(),
+      peso = base::as.integer(peso.per),
+      sexo = forcats::as_factor(p12),
+      edad = base::as.integer(p13),
+
+      ## Uso de TIC
+      uso_celular = forcats::as_factor(p23),
+      uso_smart_phone = forcats::as_factor(p23_1),
+      uso_smart_phone = forcats::fct_collapse(
+         .f = uso_smart_phone,
+         "Sí" = "Sí",
+         "No" = "No",
+         other_level = "Ns/Nc"
+      )
+   )
+
+x %>%
+   group_by(
+      uso_smart_phone
+   ) %>%
+   tally() %>%
+   plotly::plot_ly(
+   x = ~uso_smart_phone,
+   y = ~n,
+   type = "bar"
+)
+
+table(eutic$p23_1, useNA = "always")
+levels(x$uso_smart_phone)
 
 #===============#
 #### THE END ####
