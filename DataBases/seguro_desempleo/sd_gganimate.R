7c089b29fc99dd44587cd727ce5e211c4b944ecb
Paula Pereda <paaupereda@gmail.com>
2020-05-15 02:33:48 -0300

    solicitudes seguro de desempleo/cotizantes

--- DataBases/seguro_desempleo/sd_gganimate.R
+++ DataBases/seguro_desempleo/sd_gganimate.R
@@ -6,8 +6,8 @@ historical_sd <- read_rds("DataBases/seguro_desempleo/historical_sd.rds")
 
 # Solicitudes de seguro de desempleo - histórico (1988-2020)
 
-plot <- ggplot(historical_sd, aes(x = date, y = Total)) +
-   geom_path(data = historical_sd, aes(x = date, y = Total, group = 1),  colour = "#7b5888") + # definte extra data set to make it static
+plot <- ggplot(historical_sd, aes(x = date, y = total_sd)) +
+   geom_path(data = historical_sd, aes(x = date, y = total_sd, group = 1),  colour = "#7b5888") + # definte extra data set to make it static
    scale_y_continuous(breaks = seq(0, 90000, by = 10000),
                       labels = scales::number_format(big.mark = ".")) +
    scale_x_date(date_breaks = "2 years", date_labels = "%Y", expand = c(0,0)) +
@@ -39,4 +39,41 @@ options(gganimate.dev_args = list(height = 4,
 animate(plot = plot,
         renderer = gifski_renderer("static/img/evolucion_sd.gif"),
         fps = 20, duration = 12, end_pause = 80)
+
+# Solicitudes de seguro de desempleo ajustado por nº cotizantes - histórico (1988-2020)
+
+plot_2 <- ggplot(historical_sd, aes(x = date, y = ratio_sd_cot)) +
+   geom_path(data = historical_sd, aes(x = date, y = ratio_sd_cot, group = 1),  colour = "#7b5888") + # definte extra data set to make it static
+   scale_y_continuous(breaks = seq(0, 90000, by = 10000),
+                      labels = scales::number_format(big.mark = ".")) +
+   scale_x_date(date_breaks = "2 years", date_labels = "%Y", expand = c(0,0)) +
+   labs(title = "Ratio entre las solicitudes mensuales de seguro de desempleo y cotizantes\n privados mensuales a la seguridad social en Uruguay (1988-2020)",
+        x = "",
+        y = "",
+        caption = "Fuente: elaboración propia en base a datos del BPS.") +
+   theme_minimal() +
+   theme(panel.grid = element_blank(),
+         panel.grid.major.y = element_line(colour = "grey95"),
+         axis.title.y = element_text(size = 8, vjust = 3),
+         axis.text.y = element_text(size = 8),
+         axis.line.x = element_line(),
+         plot.title = element_text(hjust = 0.5, face = "bold"),
+         plot.subtitle = element_text(colour = "grey30", hjust = 0.5),
+         plot.caption = element_text(colour = "grey50"),
+         text = element_text(family = "Arial Narrow")) +
+   transition_reveal(date) +
+   view_follow(fixed_y = TRUE, fixed_x = TRUE) +
+   coord_cartesian(clip = 'off') +
+   ease_aes('cubic-in-out')
+
+options(gganimate.dev_args = list(height = 4,
+                                  width = 4*1.777778,
+                                  units = 'in',
+                                  type = "cairo",
+                                  res = 144))
+
+animate(plot = plot_2,
+        renderer = gifski_renderer("static/img/evolucion_ratio_sd_cot.gif"),
+        fps = 20, duration = 12, end_pause = 80)
+
 
